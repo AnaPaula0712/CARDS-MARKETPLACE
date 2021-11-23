5bcce85cbb8337de711b1531c2ba91745f648254
Tsubasa Yamauchi <tsubasa.yamauchi1102@gmail.com>
2021-11-23 18:45:09 -0300

    buy_card_function created but we should make condition to let card available=false

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,18 +1,24 @@
 class OrdersController < ApplicationController
-
+  
   def new
+    @card = Card.find(params[:card_id])
     @order = Order.new
   end
 
   def create
-    raise
-    user = User.find(params[:user_id])
     card = Card.find(params[:card_id])
-    order = Order.new(user: user, card: card)
+    order = Order.new(order_params)
+    order.card = card
     if order.save
       redirect_to cards_path
     else
       render :new
     end
+  end
+
+  private
+
+  def order_params
+    params.require('order').permit(:user_id)
   end
 end
