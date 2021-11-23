59c498ee9f2e31adbb2e54764a579ee236ca3dbd
victor augusto <victorfonsecass@outlook.com>
2021-11-23 11:22:49 -0300

    finalziado

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(email: "victorlosans@outlook.com",encrypted_password: "1234567" )
