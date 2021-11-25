4287f97413cbaf83711a1b910bfd57b6f9717d65
Ana Westphalen <campos1984@hotmail.com>
2021-11-25 13:57:29 -0300

    search implementado

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -23,7 +23,7 @@ class CardsController < ApplicationController
   end
 
   def search
-    # @cards = Card.where(:name ILIKE?, name: params[:name])
+    @cards = Card.search_by_name(params[:name])
   end
 
   def destroy
